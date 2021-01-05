b7988abb616a79224765ea3cfe2b1b3b97486a82
Shilong <daishilong1236@gmail.com>
2021-01-05 00:56:23 -0800

    add arc in fastest_descent_line

--- fastest_descent_line.py
+++ fastest_descent_line.py
@@ -11,7 +11,7 @@ class C:
     dt = 0.1 # [s], time step
 
 def invert_cycloid():
-    phi = np.linspace(0, C.phi, 100)
+    phi = np.linspace(0, C.phi, 1000)
     x = C.r*(phi-np.sin(phi))
     y = -C.r*(1-np.cos(phi))
     return x, y
@@ -28,8 +28,21 @@ def vertical_horizon_line():
     x_start, y_start = 0, 0
     x_end = C.r*(C.phi-np.sin(C.phi))
     y_end = -C.r*(1-np.cos(C.phi))
-    x = np.concatenate([np.zeros(50), np.linspace(x_start, x_end, 50)])
-    y = np.concatenate([np.linspace(y_start, y_end, 50), np.zeros(50)+y_end])
+    x = np.concatenate([np.zeros(500), np.linspace(x_start, x_end, 500)])
+    y = np.concatenate([np.linspace(y_start, y_end, 500), np.zeros(500)+y_end])
+    return x, y
+
+def arc_curve():
+    x_start, y_start = 0, 0
+    x_end = C.r*(C.phi-np.sin(C.phi))
+    y_end = -C.r*(1-np.cos(C.phi))
+    theta = np.arctan2(-y_end,x_end)
+    phi = np.pi-2*theta
+    r = np.hypot(x_end,y_end)/2/np.cos(theta)
+    xc, yc = r, 0
+    angles = np.linspace(np.pi, np.pi+phi, 1000)
+    x = np.cos(angles)*r + xc
+    y = np.sin(angles)*r + yc
     return x, y
 
 def simu(x, y):
@@ -59,12 +72,14 @@ def main():
     x_ic, y_ic = invert_cycloid()
     x_st, y_st = straight_line()
     x_vh, y_vh = vertical_horizon_line()
+    x_ar, y_ar = arc_curve()
     
     # trajectories
     t_ic = simu(x_ic, y_ic)
     t_st = simu(x_st, y_st)
     t_vh = simu(x_vh, y_vh)
-    max_t = np.max([t_ic[-1], t_st[-1], t_vh[-1]])
+    t_ar = simu(x_ar, y_ar)
+    max_t = np.max([t_ic[-1], t_st[-1], t_vh[-1], t_ar[-1]])
     print([t_ic[-1], t_st[-1], t_vh[-1]])
     imgs = []
     for t_ref in np.arange(0, max_t+C.dt, C.dt):
@@ -73,11 +88,14 @@ def main():
         plt.plot(x_ic, y_ic, c='tab:red')
         plt.plot(x_st, y_st, c='tab:blue')
         plt.plot(x_vh, y_vh, c='tab:blue')
+        plt.plot(x_ar, y_ar, c='tab:blue')
         x,y,t = find_closet(t_ref, x_ic, y_ic, t_ic)
         plt.plot(x, y+0.5, '-o', c='tab:red')
         x,y,t = find_closet(t_ref, x_st, y_st, t_st)
         plt.plot(x, y+0.5, '-o', c='tab:blue')
         x,y,t = find_closet(t_ref, x_vh, y_vh, t_vh)
+        plt.plot(x, y+0.5, '-o', c='tab:blue')
+        x,y,t = find_closet(t_ref, x_ar, y_ar, t_ar)
         plt.plot(x, y+0.5, '-o', c='tab:blue')
 
         plt.axis('equal')
